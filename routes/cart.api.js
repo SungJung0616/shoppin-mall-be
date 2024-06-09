1d0ef3defcb8aafd6e3a6796c7a95b4060a873d9
SungJung <sungung0616@gmail.com>
2024-06-09 11:10:56 +0900

    Add cart item deletion and quantity update functionality, display cart item count in the navbar, and update CartPage to handle empty and populated states

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -9,4 +9,11 @@ router.post('/',authController.authenticate, cartController.addItemToCart)
                 
 router.get('/', authController.authenticate, cartController.getCart)
 
+router.get("/qty", authController.authenticate, cartController.getCartQty);
+
+router.delete("/:id", authController.authenticate, cartController.deleteCartItem);
+
+router.put("/:id", authController.authenticate, cartController.editCartItem);
+
+
 module.exports = router;
